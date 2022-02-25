20a5b67ffe19f7d9cddf3a3354665a915116937b
Abrão Astério Junior <abcorinthians@gmail.com>
2022-02-25 18:48:29 -0300

    Implementing dates to tasks

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import FinishButton from "./components/finishTask.js";
 import DeleteButton from "./components/deleteTask.js"
 
-const createTask = (event) => {
+const handleNewItem = (event) => {
 
     event.preventDefault();
 
@@ -9,18 +9,36 @@ const createTask = (event) => {
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
 
+    const calendar = document.querySelector("[data-form-date]");
+    const date = moment(calendar.value);
+    const formatedDate = date.format('DD/MM/YYYY');
+
+    const data = {
+        value,
+        formatedDate
+    }
+
+    const task = createTask(data);
+
+    list.appendChild(task);
+    input.value = "";
+
+}
+
+const createTask = ({ value, formatedDate }) => {
+
     const task = document.createElement("li");
     task.classList.add("task");
-    const content = `<p class="content">${value}</p>`;
+    const content = `<p class="content">${formatedDate} * ${value}</p>`;
 
     task.innerHTML = content;
 
     task.appendChild(FinishButton());
     task.appendChild(DeleteButton());;
-    list.appendChild(task);
-    input.value = "";
+
+    return task;
 }
 
 const newTask = document.querySelector("[data-form-button]");
 
-newTask.addEventListener("click", createTask);
+newTask.addEventListener("click", handleNewItem);;
